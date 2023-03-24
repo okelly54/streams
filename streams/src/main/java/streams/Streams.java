5553f9930bc679ca0e2eb228eb0c33ab07818ce2
Olivia Kelly <126669337+okelly54@users.noreply.github.com>
2023-03-24 12:56:44 +0000

    Add files via upload

--- streams/src/main/java/streams/Streams.java
+++ streams/src/main/java/streams/Streams.java
@@ -1,5 +1,24 @@
 package streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Streams {
+
+	public static void main(String[] args) {
+
+		List<String> names = new ArrayList<>();
+		names.add("Michael");
+		names.add("Dean");
+		names.add("James");
+		names.add("Chris");
+		for (int i = 0; i < names.size(); i++) {
+			if (names[i] != "James") {
+				names.add(i - 1, "Hello");
+			}
+		}
+		names.stream().forEach(i -> System.out.println(i));
+
+	}
 
 }
